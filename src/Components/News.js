bd31eea6d38b663055d87e4d3d65606c845684ca
Digambar Tupurwadi <digz@kali>
2021-11-19 16:28:25 +0530

    created the new page with articles & next&previous btns

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,19 +2,70 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 
 export class News extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      page : 1,
+      loading: false,
+    };
+  }
+
+  async componentDidMount(){
+    let url = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=69278872befb40d381a1342f606c0edd&page=1&pageSize=12';
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles : parsedData.articles,
+      totalResults : parsedData.totalResults
+    })
+  }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=69278872befb40d381a1342f606c0edd&page=${this.state.page-1}&pageSize=12`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles : parsedData.articles2,
+      page : this.state.page-1
+    })
+    console.log(this.state.page)
+  }
+
+  handleNextClick = async () => {
+    if (this.state.page <= Math.ceil(this.state.totalResults/12)){
+      let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=69278872befb40d381a1342f606c0edd&page=${this.state.page+1}&pageSize=12`; 
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        articles : parsedData.articles, 
+        page : this.state.page+1
+      })
+      console.log(this.state.page)
+    }
+  }
+
   render() {
     return (
-      <div className='container my-3'>
-          <h2 style={{textDecoration:'underline',marginTop:'4rem'}}>Headlines for the day</h2>
-          <div className="row">
-              <div className="col-md-4 my-3"><Newsitem/></div>
-              <div className="col-md-4 my-3"><Newsitem/></div>
-              <div className="col-md-4 my-3"><Newsitem/></div>
-              <div className="col-md-4 my-3"><Newsitem/></div>
-              <div className="col-md-4 my-3"><Newsitem/></div>
-              <div className="col-md-4 my-3"><Newsitem/></div>
-              <div className="col-md-4 my-3"><Newsitem/></div>
+      <div className="container my-3">
+        <h2 style={{ textDecoration: "underline", marginTop: "4rem" }}>
+          Headlines for the day
+        </h2>
+        
+        <div className="row">
+          {this.state.articles.map((element)=>{
+          return <div className="col-md-4 my-3" key={element.url}>
+            <Newsitem title={element.title.slice(0,60)+'...'} desc={element.description.slice(0,130)} image = {element.urlToImage} url={element.url} />
           </div>
+          })}
+        </div>
+
+        <div className="container d-flex justify-content-between">
+          <button className="btn btn-primary" disabled={this.state.page<=1} onClick={this.handlePrevClick}>&larr;Previous</button>
+          <button className="btn btn-primary" onClick={this.handleNextClick}>Next&rarr;</button>
+        </div>
+
       </div>
     );
   }
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,18 +2,21 @@ import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
+
+    // extracting some fields|variables from props
+    let {title,desc,image,url} = this.props;
+
     return (
       <>
         <div className="card" style={{width: "18rem"}}>
-          <img src='...' className="card-img-top" alt="..." />
+          <img src={image} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">{title}</h5>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              {desc}
             </p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <a href={url} rel="noreferrer" target='_blank' style={{textDecoration:"none"}}>
+              read more...
             </a>
           </div>
         </div>
