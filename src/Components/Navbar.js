5ac3cec2debff38f567f32915ee82c7174b075ec
Digambar Tupurwadi <digz@kali>
2021-11-21 15:31:38 +0530

    Converted Navbar, Newsitem & Spinner components from class based to function based

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,57 +1,55 @@
-import React, { Component } from "react";
-import { Link  } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              NewsChimp
-            </Link>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link " aria-current="page" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/business">
-                  Business
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/entertainment">
-                  Entertainment
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/health">
-                  Health
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/science">
-                  Science
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/sports">
-                  Sports
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/technology">
-                  Technology
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </>
-    );
-  }
-}
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            NewsChimp
+          </Link>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link " aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/business">
+                Business
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/entertainment">
+                Entertainment
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/health">
+                Health
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/science">
+                Science
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/sports">
+                Sports
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/technology">
+                Technology
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,34 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Newsitem extends Component {
-  render() {
-    // extracting some fields|variables from props
-    let { title, desc, image, url, author, time, source } = this.props;
+const Newsitem = (props) => {
 
-    return (
-      <>
-        <div className="card">
-          <img src={image} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{desc}</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {author} at {time}
-              </small>
-            </p>
-            <a href={url} rel="noreferrer" target="_blank">
-              read more...
-            </a>
-          </div>
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {source}
-            <span className="visually-hidden">unread messages</span>
-          </span>
+  // extracting some fields|variables from props
+  let { title, desc, image, url, author, time, source } = props;
+
+  return (
+    <>
+      <div className="card">
+        <img src={image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{desc}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {author} at {time}
+            </small>
+          </p>
+          <a href={url} rel="noreferrer" target="_blank">
+            read more...
+          </a>
         </div>
-      </>
-    );
-  }
-}
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          {source}
+          <span className="visually-hidden">unread messages</span>
+        </span>
+      </div>
+    </>
+  );
+};
 
 export default Newsitem;
